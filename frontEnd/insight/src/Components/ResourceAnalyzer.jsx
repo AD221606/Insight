bc43b7c45794f7f5ca71759f6bf2c166ef09ff8e
AD221606 <aryandubey221606@gmail.com>
2025-12-10 13:35:15 +0530

    Delete frontEnd directory

--- frontEnd/insight/src/Components/ResourceAnalyzer.jsx
+++
@@ -1,8 +0,0 @@
-export default function ResourceAnalyser() {
-    return(
-        <>
-            <input type="file" />
-            <input type="text" placeholder="Enter your requirements from this material"/>
-        </>
-    );
-}
